95b5118918072b73dd11cf63dcac3c0bdb2102b0
Jomo245 <johnisige2020@gmail.com>
2025-04-14 18:59:00 +0300

    variables

---
+++ index.js
@@ -0,0 +1,10 @@
+//console.log("Hello World");
+//console.log("I like pizza");
+
+//window.alert("This is an alert");
+//window.alert("I like pizza");
+
+//THIS IS SINGLE LINE COMMENT
+
+//document.getElementById("myh1").textContent="Hell John";
+//document.getElementById("myp").textContent="This is my true journey to Javascript coding";

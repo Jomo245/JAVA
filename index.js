412acf401bd3b8ceb87669172e2bdb1ddb0b77bd
Jomo245 <johnisige2020@gmail.com>
2025-04-16 22:10:08 +0300

    increase, decrease and reset

--- index.js
+++ index.js
@@ -8,3 +8,79 @@
 
 //document.getElementById("myh1").textContent="Hell John";
 //document.getElementById("myp").textContent="This is my true journey to Javascript coding";
+
+
+//let quantity=20;
+//let price=100;
+
+//let total_price;
+
+//total_price=quantity*price;
+
+//let FirstName="Jomo";
+
+//console.log(FirstName);
+
+//console.log(typeof total_price);
+
+
+
+//let FullName="John Jomo";
+//let age=25;
+//let isStudent=true;
+
+//document.getElementById("p1").textContent= FullName;
+//document.getElementById("p2").textContent=age;
+//document.getElementById("p3").textContent= isStudent;
+
+
+
+
+
+
+
+
+//ARITHMETIC OPERATORS
+
+//let students=20;
+
+//students=students+1;
+//students=students/2;
+//students=students-2;
+//students=students*2;
+
+//console.log(students);
+
+
+
+
+//let username;
+
+//document.getElementById("mysubmit").onclick=function(){
+  //username=document.getElementById("mytext").value;
+  //document.getElementById("myH2").textContent=username
+  
+   
+  //console.log(username)
+
+
+const decrease=document.getElementById("decrease");
+const reset=document.getElementById("reset");
+const increase=document.getElementById("increase");
+const mycount=document.getElementById("mycount")
+let count=0;
+
+increase.onclick=function(){
+  count++;
+  mycount.textContent =count;
+}
+
+
+
+
+
+
+
+
+
+
